5d74af5c8b23f23795613f6af0c75b2266182101
John Canty <jtcanty117@berkeley.edu>
2015-08-24 18:40:10 -0700

    update to iterate over multiple files

--- tif2dax.m
+++ tif2dax.m
@@ -1,21 +1,26 @@
 % John Canty                                    Created 08/23/15
 % Yildiz Lab
 
-% Converts a .tiff file to a .dax file
+% Converts a .tiff file to a .dax file. Navigate workspace to directory.
 % Last modified: 08/23/15
 
 clear all;
 
 % Obtain list of files in file directory
 dirData = dir('*.tif');
+num = length(dirData);
+data = [];
+PathName = pwd;
 
-for file = dirData:
-    
-    info = imfinfo(file);
-    num_img = numel(info);
+for i = 1:num
+    % Identify image name and num of frames 
+    field = dirData(i);
+    fname = field.name;
+    info = imfinfo(fname);
+    num_frame = numel(info);
 
     % Create 1-D array of all image pixels 
-    for k = 1:num_img
+    for k = 1:num_frame
         curr_img = imread(fname, k, 'Info' ,info);
         data = vertcat(data, reshape(rot90(curr_img,2),size(curr_img,1)*size(curr_img,2),1));
     end
@@ -27,13 +32,15 @@ for file = dirData:
     fnew = fopen(dax_name,'w','ieee-be');
     fwrite(fnew,data,'uint16',0,'ieee-be');
     fclose(fnew);
-
+    
+    data = [];
+    
     finfo = strcat(fname,'inf');
     fileID = fopen(finfo,'w');
     fprintf(fileID,'file path = %s\r\n',PathName);
-    fprintf(fileID,'number of frames = %d\r\n',num_images);
+    fprintf(fileID,'number of frames = %d\r\n',num_frame);
     fprintf(fileID,'binning = 1 x 1\r\n');
-    fprintf(fileID,'frame dimensions = %d x %d',size(A,1),size(A,2));
+    fprintf(fileID,'frame dimensions = %d x %d',size(curr_img,1),size(curr_img,2));
     fclose(fileID);
 
 end
