766cd0bd3368de42bd020cc917e71b2d41573030
John Canty <jtcanty117@berkeley.edu>
2015-08-24 21:16:23 -0700

    Extract spot ints from Insight3 output

---
+++ ExtractInts.m
@@ -0,0 +1,8 @@
+% John Canty                                      Created 08/24/15
+% Yildiz Lab
+
+% Takes outputted .txt file from .dax ROI of Z-stack and extracts the
+% intensity values. Outputs values as an excel file.
+
+% Modified 08/24/15
+
