e19dbb8ca83ce9b4b8fae38c4dda4a87aced08ba
John Canty <jtcanty117@berkeley.edu>
2015-08-24 23:59:34 -0700

    generate list of spot intensities from z-stacks

--- ExtractInts.m
+++ ExtractInts.m
@@ -6,3 +6,21 @@
 
 % Modified 08/24/15
 
+clear all;
+
+dirData = dir('*.txt');
+num = length(dirData);
+int_list = [];
+
+for i = 1:num
+    field = dirData(i);
+    fname = field.name;
+    info = tdfread(fname,'\t');
+    ints = info.I;
+    int_list = vertcat(int_list,ints);
+end
+
+% Write to excel file
+name = input('Input file name: ','s');
+filename = strcat(name,'.xlsx');
+xlswrite(filename,int_list);
