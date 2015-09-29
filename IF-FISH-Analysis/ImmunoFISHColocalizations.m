0f7eed37b5aa0cc67efa18139486fbe8410025d6
John Canty <jtcanty117@berkeley.edu>
2015-09-28 20:09:47 -0700

    Initial commit

---
+++ IF-FISH-Analysis/ImmunoFISHColocalizations.m
@@ -0,0 +1,7 @@
+% John Canty                                Created: 09/2815
+% Yildiz Lab
+
+
+% Identifies Z-stack coordinates for spots in the IF-FISH channels and then
+% determines which spots colocalize. Takes input TIF files.
+
